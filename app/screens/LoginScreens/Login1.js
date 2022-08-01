1108f861fbb5102e4b7cfb6758847bf3b8ea63c9
Saitoria <saronisaitoria@gmail.com>
2022-08-01 16:11:04 +0300

    App file structure

---
+++ app/screens/LoginScreens/Login1.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet,View,Text } from 'react-native';
+import App from '../../../App';
+
+function App()
+{
+    return(
+        <View>
+            <Text>Login 1 screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
