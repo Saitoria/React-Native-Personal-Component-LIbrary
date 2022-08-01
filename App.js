e7962ec72911ba49f3dab5bfc32597a56b102c6c
Saitoria <saronisaitoria@gmail.com>
2022-08-01 15:54:08 +0300

    Initial files

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet,Text,View} from 'react-native';
+
+export default function App()
+{
+  return(
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    padding:10,
+  }
+});
