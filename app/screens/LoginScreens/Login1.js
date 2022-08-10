8df8de2226c70ad6c10658828c5f3604c09f2f2c
Saitoria <saronisaitoria@gmail.com>
2022-08-10 10:22:01 +0300

    Login screen 1

--- app/screens/LoginScreens/Login1.js
+++ app/screens/LoginScreens/Login1.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { StyleSheet,View,Text } from 'react-native';
-import App from '../../../App';
+import { StyleSheet,View,Text,ImageBackground} from 'react-native';
 
-function App()
+//Login screen 0 with a background screen
+function Login1()
 {
     return(
-        <View>
-            <Text>Login 1 screen</Text>
-        </View>
+        <ImageBackground source={require('../../resources/images/login0.jpg')} resizeMode="cover" style={styles.imgBackground}>
+            <View style={styles.topContainer}>
+                <Text style={styles.header1}>HELLO WORLD</Text>
+            </View>
+            <View>
+
+            </View>
+        </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
+    imgBackground:{
+        width:'100%',
+        height:'100%',
+    },
+    topContainer:{
+        position:'absolute',
+        top:'30%',
+    },
+    bottomContainer:{
 
+    },
+    header1:{
+        fontSize:30,
+        fontWeight:'bold',
+    }
 });
 
-export default App;
+export default Login1;
