2578674de2280809a76ea71f0147bcc03bf70e3e
Saitoria <saronisaitoria@gmail.com>
2022-09-20 10:58:11 +0300

    Changes to App.js

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet,Text,View} from 'react-native';
 import Login1 from './app/screens/LoginScreens/Login1';
-import Signup from './app/screens/SignupScreens/Signup';
+import Signup from './app/screens/SignupScreens/Signup1';
+import Home from './app/screens/HomeScreens/Home1';
+import Description from './app/screens/DescriptionScreens/Description1';
 
 export default function App()
 {
   return(
    //<Login1/>
-    <Signup/>
+    //<Signup/>
+    <Home/>
+    //<Description/>
   )
 }
 
